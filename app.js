e4a864bf88fddc9c335cfc109c5fe284cdc4b8da
gabymq <gmorenoqaby@gmail.com>
2018-10-02 16:11:39 -0500

    Seccion 11 terminada

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ var bodyParser = require('body-parser');
 
 var appRoutes = require('./routes/app');
 var usuarioRoutes = require('./routes/usuario');
+var hospitalRoutes = require('./routes/hospital');
 var loginRoutes = require('./routes/login');
+var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
+var imagenesRoutes = require('./routes/imagenes');
 
 
 // Inicializar variables
@@ -27,11 +32,21 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err,res ) =
 
 });
 
+// Server index config
+// var serveIndex = require('serve-index');
+// app.use(express.static(__dirname + '/'))
+// app.use('/uploads', serveIndex(__dirname + '/uploads'));
 
 // Rutas
 
 app.use('/usuario', usuarioRoutes);
+app.use('/hospital',hospitalRoutes);
+app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
+app.use('/img', imagenesRoutes);
+
 app.use('/', appRoutes);
 
 
